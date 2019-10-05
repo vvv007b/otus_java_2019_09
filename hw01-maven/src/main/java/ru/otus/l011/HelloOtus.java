0258abb1c6c4f2351f1dba0048d5a92bdd0045a4
vvv007b <vv007b_ne@mail.ru>
2019-10-06 02:01:25 +0300

    HW1 comment

---
+++ hw01-maven/src/main/java/ru/otus/l011/HelloOtus.java
@@ -0,0 +1,19 @@
+package ru.otus.l011;
+
+import com.google.common.collect.Lists;
+
+import java.util.*;
+
+public class HelloOtus {
+
+    public static void main(String[] args) {
+
+        List<String> arrayList = new ArrayList<>();
+        arrayList.add("Hello");
+        Collections.shuffle(arrayList);
+        System.out.println(Lists.reverse(arrayList));
+
+
+    }
+
+}
